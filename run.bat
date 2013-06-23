cee0f3e9c5905b056350e9e47041997d098567d7
Shani Raba <rabashani@gmail.com>
2013-06-23 15:30:07 +0300

    run will send the host+port
    server.py is getting args from the command line

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 del /F /S /Q public
 xcopy src\app\thewall  public  /E
-python src\server\bottle\server.py
+python src\server\bottle\server.py localhost 3000
--- src/server/bottle/server.py
+++ src/server/bottle/server.py
@@ -1,3 +1,4 @@
+import sys, string, os
 from bottle import route, run, template
 from bottle import static_file
 from bottle import error
@@ -12,4 +13,22 @@ def error404(error):
     return 'Nothing here, sorry'
 
 
-run(host='0.0.0.0', port=3000)
+
+
+## running the server (+command lines...)
+print "#############"
+print "## in case you want to push your own host\port just use the command line - when running the server."
+print "## argv[1] = host, argv[2] = port"
+print "#############"
+
+host = '0.0.0.0'
+if len(sys.argv) > 1:
+	host = sys.argv[1]
+	print 'argument #1: ' + sys.argv[1]
+
+port = '3000'
+if len(sys.argv) > 2:
+	port = sys.argv[2]
+	print 'argument #2: ' + sys.argv[2]
+
+run(host=host, port=port)
