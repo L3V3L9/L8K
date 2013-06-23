1f7ca0eb1d73b36720e7a53f847a6a29eb6ebde2
Ithai Levi <ithai.levi@gmail.com>
2013-06-23 11:56:30 +0000

    delivering our first working wall

--- src/server/bottle/server.py
+++ src/server/bottle/server.py
@@ -2,29 +2,14 @@ from bottle import route, run, template
 from bottle import static_file
 from bottle import error
 
-@route('/')
-def index():
-	return static_file('index.html', root='public/')
-
-@route('/help')
-def help():
-	return static_file('index.html', root='public/help/')
-
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
-    return static_file(filepath, root='public/static/')
-
-
-@route('/hello/')
-@route('/hello/<name>')
-def Hello(name='World'):
-    return template('<b>Hello {{name}}</b>!', name=name)
-
+    return static_file(filepath, root='public/')
 
 @error(404)
 def error404(error):
     return 'Nothing here, sorry'
 
 
-run(host='localhost', port=3000)
+run(host='0.0.0.0', port=3000)
