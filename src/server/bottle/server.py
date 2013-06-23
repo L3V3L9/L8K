0bb5286a2d9e4f9f78a8ca3c7c0acf389e4b6e70
Shani Raba <rabashani@gmail.com>
2013-06-23 13:56:42 +0300

    changing the server.py to route to static and to help pages

--- src/server/bottle/server.py
+++ src/server/bottle/server.py
@@ -1,12 +1,35 @@
 from bottle import route, run, template
+from bottle import static_file
+from bottle import error
+
+@route('/')
+def index():
+	return template('Hola')
 
 
 @route('/hello/')
-def index(name='World'):
+@route('/hello/<name>')
+def Hello(name='World'):
     return template('<b>Hello {{name}}</b>!', name=name)
 
-@route('/hello/<name>')
-def indexWithParam(name='World'):
-    return template('<b>Hello {{name}}</b>!', name=name)
+
+@route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='./static/')
+
+@route('/views/<filepath:path>')
+def server_views(filepath):
+    return static_file(filepath, root='./views/')
+
+
+@route('/help')
+def help():
+	return static_file('index.html', root='views/help/')
+
+
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry'
+
 
 run(host='localhost', port=8080)
