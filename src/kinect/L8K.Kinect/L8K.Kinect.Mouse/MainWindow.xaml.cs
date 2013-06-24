5c64d4aebce9194c59ffd2367622e66bf0df12cb
yossi <yossi@delver.com>
2013-06-24 09:49:55 +0300

    Added solution for Kinect interface

---
+++ src/kinect/L8K.Kinect/L8K.Kinect.Mouse/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace L8K.Kinect.Mouse
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+		{
+			
+		}
+
+		private void MainWindow_OnClosed(object sender, EventArgs e)
+		{
+		}		
+	}
+}
